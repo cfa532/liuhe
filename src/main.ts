9bc8228103203821b8a5016a6065fb4aa825ddef
cfa532 <cfa532@yahoo.com>
2024-02-26 08:55:04 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,14 @@ const app = createApp(App)
 app.use(createPinia())
 leitherBackend();   // init backend after Pinia
 app.use(router)
-app.config.errorHandler = (err, instance, info) => {
+app.config.errorHandler = async (err, instance, info) => {
     // Handle the error globally
     console.error("Global error:", err);
     console.log("Vue instance:", instance);
     console.log("Error info:", info);
+
+    sessionStorage.removeItem("sid")
+    await lapi.login()
 }
 console.warn("main.ts built....on " + __BUILD_TIME__, "ver:"+import.meta.env.VITE_APP_VERSION)
 const lapi = useLeitherStore()
