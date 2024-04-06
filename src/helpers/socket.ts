23c040f1acde6534796a258e9feea044b7f23287
cfa532 <cfa532@yahoo.com>
2024-04-06 21:47:03 +0800

    socketio cannot stream

---
+++ src/helpers/socket.ts
@@ -0,0 +1,35 @@
+import { reactive } from "vue";
+import { io } from "socket.io-client";
+
+export const state = reactive({
+    connected: false,
+    fooEvents: [] as any[],
+    barEvents: [] as any[]
+});
+
+// "undefined" means the URL will be computed from the `window.location` object
+// const URL = "http://localhost:8080";
+const URL = "http://125.229.161.122:4800";
+
+export const socket = io(URL, {
+    withCredentials: true,
+    autoConnect: false
+  });
+
+// socket.on("connect", () => {
+//     console.log("Connected")
+//     state.connected = true;
+// });
+
+// socket.on("disconnect", () => {
+//     console.log("Disconnected")
+//     state.connected = false;
+// });
+
+// socket.on("foo", (...args) => {
+//     state.fooEvents.push(args);
+// });
+
+// socket.on("bar", (...args) => {
+//     state.barEvents.push(args);
+// });
---
+++ src/views/cases/SocketIO.vue
@@ -0,0 +1,149 @@
+<script setup lang="ts">
+import { onErrorCaptured, onMounted, ref, watch } from 'vue';
+import { useAuthStore, useCaseStore, useCaseListStore } from '@/stores';
+import { useRoute } from 'vue-router';
+import { Share } from '@/components'
+import { socket, state } from '@/helpers'
+
+const emits = defineEmits(["newCaseId"])     // add new case to list
+const caseList = useCaseListStore()
+const caseStore = useCaseStore()
+const { user } = useAuthStore()
+const route = useRoute();
+const query = ref()
+const stream_in = ref("")
+const spinner = ref("提交")
+const btnSubmit = ref()
+const chatHistory = ref<ChatItem[]>([])
+
+async function onSubmit() {
+    const timer = window.setTimeout(()=>{
+        // alert user to reload
+        window.alert("如果等待超时，尝试刷新页面后重新提交。")
+        spinner.value = "提交"
+        btnSubmit.value.disabled = false
+    }, 120000)
+
+    socket.connect();
+    socket.on("stream", token => {
+        console.log(token)
+        stream_in.value += token;
+    })
+    socket.on("error", (err) => {
+        console.error(err)
+        spinner.value = "提交"
+        btnSubmit.value.disabled = false
+        socket.disconnect()
+    })
+    socket.on("connect", ()=>{
+        // send message to websoceket and wait for response
+        console.log("Submit query to AI: ", query.value)
+        const ci = {} as ChatItem
+        query.value = typeof query.value =="undefined" ? "Hello" : query.value;        // query submitted to AI
+        ci.Q = query.value
+        ci.A = ""
+        console.log(ci)
+
+        const qwh: any = {query: ci.Q, history: [] as Array<ChatItem>}   // query with history
+        for (let i=0; i<Math.min(6, chatHistory.value.length); i++) {
+            qwh.history.push(chatHistory.value[i])
+        }
+        
+        spinner.value = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>'
+        btnSubmit.value.disabled = true
+        stream_in.value = ""
+        socket.emit("query", ({input: qwh, parameters: user.template}),  (ret: any) => {
+            console.log("Ws received:", ret)
+            window.clearTimeout(timer)
+            const ci = {} as ChatItem
+            ci.Q = query.value
+            ci.A = ret
+            chatHistory.value!.unshift(ci)
+            caseStore.addChatItem(ci)
+            query.value = ""
+            stream_in.value = ""
+            spinner.value = "提交"
+            btnSubmit.value.disabled = false
+            socket.disconnect()
+        })
+    })
+}
+onMounted(async ()=>{
+    // load chat history of a particular case
+    await caseStore.initCase(route.params.id as string)
+    console.log("Case Mounted", caseStore.$state)
+    chatHistory.value = caseStore.chatHistory
+})
+watch(()=>route.params.id, async (nv, ov)=>{
+    // force changing of user case
+    if (nv && nv!=ov) {
+        await caseStore.initCase(nv as string)
+        chatHistory.value = caseStore.chatHistory
+        console.log(caseStore.case)
+    }},
+)
+onErrorCaptured((err)=>{
+    console.error(err)
+    return false
+})
+async function deletePost() {
+    await caseList.deleteCase(route.params.id as string)
+    emits("newCaseId", "0")
+}
+</script>
+
+<template>
+<div class="col-md-10 col-sm-12">
+<form>
+<div class="container d-grid row-gap-3">
+    <Share style=" display: inline-block; position: absolute; right:40px;" @delete-post="deletePost"></Share>
+
+    <div class="row mt-2">
+        <textarea class="form-control" rows="5" v-model="query" placeholder="Ask me...."></textarea>
+        <p></p>
+        <div class="col">
+            <button ref="btnSubmit" @click.prevent="onSubmit" type="button" style="position: relative; float: right;" class="btn btn-primary" v-html="spinner"></button>
+        </div>
+    </div>
+</div>
+</form>
+
+<div class="row text-secondary mt-4">
+    <div v-if="stream_in.length>0">
+        <div style="white-space: pre-wrap; color: black;"><label>A:&nbsp;</label>{{ stream_in }}</div>
+        <p></p>
+        <hr/>
+    </div>
+    <div style="margin-left: 1px;" v-for="(ci, index) in chatHistory" :key="index">
+        <div class="Q"><label>Q:&nbsp;</label>{{ ci.Q }}</div>
+        <div class="A"><label>A:&nbsp;</label>{{ ci.A }}</div>
+        <p></p>
+        <hr/>
+    </div>
+</div>
+</div>
+</template>
+<style>
+div.Q {
+    background-color:rgb(238, 238, 213);
+    /* border: 1px solid blue; */
+    border-radius: 10px;
+    padding: 5px;
+    margin-bottom: 10px;
+
+}
+div.A {
+    white-space: pre-wrap;
+    color: rgb(33, 31, 31);
+}
+.col-12 {
+    background-color: rgb(199, 187, 163);
+    padding: 5px;
+    border-radius: 5px;
+    /* border: 1px solid #73AD21; */
+}
+.text-secondary {
+    /* --bs-text-opacity: 1; */
+    color: rgb(53 57 62) !important;
+}
+</style>
